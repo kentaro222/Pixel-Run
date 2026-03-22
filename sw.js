6ca7209e111e7130df1258adb637ca35cd090a01
kentaro222 <magdracky@gmail.com>
2026-03-22 14:38:09 +0900

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'pixel-run-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './player_1bit00_sheet.png',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+// インストール時にファイルをキャッシュ
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+// オフライン時はキャッシュから応答
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
